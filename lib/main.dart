2819000a1ee16573c50578efc6c845e131b44c9f
Itsuki Aoyagi <mail@yagi2.dev>
2021-12-01 17:30:13 +0900

    apply InheritedWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'dart:convert' show json;
 
 import 'package:flutter/material.dart';
-import 'package:flutter_tutorial_github/github_repository_detail_page.dart';
-import 'package:flutter_tutorial_github/model/github_repository.dart';
 import 'package:http/http.dart' as http;
+
+import 'github_repository_detail_page.dart';
+import 'model/github_repository.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,60 +13,39 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return const MaterialApp(
+      home: _StatefulInherited(
+        child: _HomePage(title: "GitHub Repository search"),
       ),
-      home: const GitHubRepositoryListPage(title: 'GitHub Repository Search'),
     );
   }
 }
 
-class GitHubRepositoryListPage extends StatefulWidget {
-  const GitHubRepositoryListPage({Key? key, required this.title})
-      : super(key: key);
+class _HomePage extends StatelessWidget {
+  const _HomePage({Key? key, required this.title}) : super(key: key);
+
   final String title;
 
   @override
-  State<GitHubRepositoryListPage> createState() =>
-      _GitHubRepositoryListPageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text(title)),
+        body: Column(
+          children: const <Widget>[
+            _GitHubRepositorySearchInputWidget(),
+            Expanded(child: _GitHubRepositoryListWithProgressWidget()),
+          ],
+        ));
+  }
 }
 
-class _GitHubRepositoryListPageState extends State<GitHubRepositoryListPage> {
-  List<GitHubRepository> _repositories = [];
-  bool _isLoading = false;
+class _GitHubRepositorySearchInputWidget extends StatelessWidget {
+  const _GitHubRepositorySearchInputWidget({Key? key}) : super(key: key);
 
-  Future<List<GitHubRepository>> _searchRepositories(String searchQuery) async {
-    setState(() {
-      _repositories = [];
-      _isLoading = true;
-    });
-
-    final response = await http.get(Uri.parse(
-        'https://api.github.com/search/repositories?q=' +
-            searchQuery +
-            "&sort=stars&order=desc"));
-
-    setState(() {
-      _isLoading = false;
-    });
-
-    if (response.statusCode == 200) {
-      List<GitHubRepository> list = [];
-      Map<String, dynamic> decoded = json.decode(response.body);
-      for (var item in decoded['items']) {
-        list.add(GitHubRepository.fromJson(item));
-      }
-      return list;
-    } else {
-      throw Exception('fail to search repositories.');
-    }
-  }
-
-  Widget _buildInput() {
+  @override
+  Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.all(16.0),
       child: TextField(
@@ -74,36 +54,48 @@ class _GitHubRepositoryListPageState extends State<GitHubRepositoryListPage> {
             hintText: "Please enter a search keyword.",
             labelText: "Search"),
         onSubmitted: (input) {
-          _searchRepositories(input).then((repositories) {
-            setState(() {
-              _repositories = repositories;
-            });
-          });
+          _StatefulInherited.of(context, listen: true)
+              .searchRepositories(input);
         },
       ),
     );
   }
+}
 
-  Widget _buildRepositoryListWithProgress() {
+class _GitHubRepositoryListWithProgressWidget extends StatelessWidget {
+  const _GitHubRepositoryListWithProgressWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
     return Stack(
       children: <Widget>[
         ListView.builder(
           scrollDirection: Axis.vertical,
           shrinkWrap: true,
           itemBuilder: (BuildContext context, int index) {
-            final repository = _repositories[index];
-            return _buildCard(repository);
+            return _GitHubRepositoryCardWidget(
+                repository: _StatefulInherited.of(context, listen: false)
+                    ._repositories[index]);
           },
-          itemCount: _repositories.length,
+          itemCount:
+              _StatefulInherited.of(context, listen: true)._repositories.length,
         ),
-        _isLoading
+        _StatefulInherited.of(context, listen: true)._isLoading
             ? const Center(child: CircularProgressIndicator())
             : Container()
       ],
     );
   }
+}
 
-  Widget _buildCard(GitHubRepository repository) {
+class _GitHubRepositoryCardWidget extends StatelessWidget {
+  const _GitHubRepositoryCardWidget({Key? key, required this.repository})
+      : super(key: key);
+
+  final GitHubRepository repository;
+
+  @override
+  Widget build(BuildContext context) {
     return Card(
         margin: const EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
         child: InkWell(
@@ -170,16 +162,84 @@ class _GitHubRepositoryListPageState extends State<GitHubRepositoryListPage> {
           ),
         ));
   }
+}
+
+class _StatefulInherited extends StatefulWidget {
+  const _StatefulInherited({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  final Widget child;
+
+  @override
+  State<StatefulWidget> createState() => _StatefulInheritedState();
+
+  static _StatefulInheritedState of(
+    BuildContext context, {
+    required bool listen,
+  }) {
+    return listen
+        ? (context.dependOnInheritedWidgetOfExactType<_Inherited>()
+                as _Inherited)
+            .state
+        : (context.getElementForInheritedWidgetOfExactType<_Inherited>()?.widget
+                as _Inherited)
+            .state;
+  }
+}
+
+class _StatefulInheritedState extends State<_StatefulInherited> {
+  List<GitHubRepository> _repositories = [];
+  bool _isLoading = false;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: Text(widget.title)),
-        body: Column(children: <Widget>[
-          _buildInput(),
-          Expanded(
-            child: _buildRepositoryListWithProgress(),
-          )
-        ]));
+    return _Inherited(
+      state: this,
+      child: widget.child,
+    );
   }
+
+  void searchRepositories(String searchQuery) async {
+    setState(() {
+      _repositories = [];
+      _isLoading = true;
+    });
+
+    final response = await http.get(Uri.parse(
+        'https://api.github.com/search/repositories?q=' +
+            searchQuery +
+            "&sort=stars&order=desc"));
+
+    setState(() {
+      _isLoading = false;
+    });
+
+    if (response.statusCode == 200) {
+      List<GitHubRepository> list = [];
+      Map<String, dynamic> decoded = json.decode(response.body);
+      for (var item in decoded['items']) {
+        list.add(GitHubRepository.fromJson(item));
+      }
+      setState(() {
+        _repositories = list;
+      });
+    } else {
+      throw Exception('fail to search repositories.');
+    }
+  }
+}
+
+class _Inherited extends InheritedWidget {
+  const _Inherited({
+    Key? key,
+    required this.state,
+    required Widget child,
+  }) : super(key: key, child: child);
+
+  final _StatefulInheritedState state;
+
+  @override
+  bool updateShouldNotify(covariant InheritedWidget oldWidget) => true;
 }
